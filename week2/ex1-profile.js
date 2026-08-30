fd74e8eb29bfddd07474d601a44a66e7b355f4bd
68112754-ops <wongspat009@gmail.com>
2026-08-30 18:56:52 +0700

    Create ex1-profile.js

---
+++ week2/ex1-profile.js
@@ -0,0 +1,17 @@
+const nickname = "teen" // สร้างตัวแปรเก็บชื่อเล่น
+const studentId = 68112754 // สร้างตัวแปรเก็บรหัสนักศึกษา
+const age = 20; // สร้างตัวแปรเก็บอายุ
+const major ="computer engineer" // สร้างตัวแปรเก็บสาขา
+const register = 6 // สร้างตัวแปรเก็บลงทะเบียน
+const yearsLeft = 2;
+const graduationYear = 2569 + yearsLeft; // คำนวณปีที่จะจบจากปีปัจจุบัน (2569)
+
+
+console.log(`=====  บัตรแนะนำตัว  =====
+ชื่อเล่น        : ${nickname}
+รหัสนักศึกษา    : ${studentId}
+อายุ            : ${age} ปี
+สาขาวิชา        : ${major}
+ลงทะเบียน       : ${register} วิชา
+ปีที่จะจบ        : ${graduationYear}
+==========================`);
